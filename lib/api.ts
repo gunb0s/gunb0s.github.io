86a956404880795c294657f07d2115859f9e7403
gunb0s <gubos1031@gmail.com>
2024-08-25 15:19:10 +0900

    init

--- lib/api.ts
+++ lib/api.ts
@@ -5,6 +5,19 @@ import { Post } from "@/interface/post";
 
 const postsDirectory = join(process.cwd(), "_posts");
 
+export function getPostSlugs() {
+  return fs.readdirSync(postsDirectory);
+}
+
+export function getAllPosts() {
+  const slugs = getPostSlugs();
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug))
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+
+  return posts;
+}
+
 export function getPostBySlug(slug: string) {
   const realSlug = slug.replace(/\.md$/, "");
   const fullPath = join(postsDirectory, `${realSlug}.md`);
